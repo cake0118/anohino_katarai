466aace938283a9a3cdc34fc818efbeab9c12352
EC2 Default User <ec2-user@ip-172-31-11-19.ap-northeast-1.compute.internal>
2024-12-10 06:31:17 +0000

    [Add]bootstrapの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations, :passwords] controllers: {
+
+  root to: 'public/homes#top'
+  get '/about', to: 'public/homes#about'
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
   devise_for :users, skip: [:passwords], controllers: {
-    registrations: "public/ragistrations",
+    registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
