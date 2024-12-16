af6ab9ba9ce0ab96fdeb7be69da924bb2691b519
EC2 Default User <ec2-user@ip-172-31-11-19.ap-northeast-1.compute.internal>
2024-12-16 10:48:20 +0000

    [update]_idのデフォルト値の削除

--- db/migrate/20241212060459_create_games.rb
+++ db/migrate/20241212060459_create_games.rb
@@ -2,8 +2,8 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
 
-      t.integer :user_id, null: false, default: ""
-      t.integer :headware_id, null: false, default: ""
+      t.integer :user_id, null: false
+      t.integer :headware_id, null: false
       t.string :title, null: false, default: ""
       t.text :body, null: false, default: ""
 
