d8c17c87fc4804d5ad1eecc5c65293c358972de9
EC2 Default User <ec2-user@ip-172-31-11-19.ap-northeast-1.compute.internal>
2024-12-22 07:41:02 +0000

    [Add]部分検索と行検索

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,10 +1,34 @@
 class Public::SearchesController < ApplicationController
 
   def index
-    if params[:first_character]
-      @games = search_by_katakana(params[:first_character])
+    @kana = params[:kana]
+    @query = params[:query]
+    
+    if @query.present?
+      @games = Game.where("title LIKE ? OR title_kana LIKE ?", "%#{@query}%", "%#{@query}%" ).order(:title_kana)
+    elsif @kana.present?
+      range = case @kana
+              when 'ア' then ['ア', 'オ']
+              when 'カ' then ['カ', 'コ']
+              when 'ガ' then ['ガ', 'ゴ']
+              when 'サ' then ['サ', 'ソ']
+              when 'ザ' then ['ザ', 'ゾ']
+              when 'タ' then ['タ', 'ト']
+              when 'ダ' then ['ダ', 'ド']
+              when 'ナ' then ['ナ', 'ノ']
+              when 'ハ' then ['ハ', 'ホ']
+              when 'バ' then ['バ', 'ボ']
+              when 'パ' then ['パ', 'ポ']
+              when 'マ' then ['マ', 'モ']
+              when 'ヤ' then ['ヤ', 'ヨ']
+              when 'ラ' then ['ラ', 'ロ']
+              when 'ワ' then ['ワ', 'ン']
+              else [@kana, @kana] # 想定外の入力があった場合、該当なし
+              end
+
+      @games = Game.where(title_kana: range[0]..range[1]).order(:title_kana)
     else
-      @games = Game.all
+      @games = Game.none # 空の結果
     end
   end
 
